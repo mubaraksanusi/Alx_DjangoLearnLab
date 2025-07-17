bb6be3267c2ccd4c3941591fa2228b8a6a7b2302
mubarak sanusi <mubarakmubi199@gmail.com>
2025-07-16 21:09:41 -0700

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import list_books  # ✅ Required by checker
-from .views import LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, register  # ✅ Includes views.register
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # ✅
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # ✅
+    path('register/', register, name='register'),  # ✅
 ]
